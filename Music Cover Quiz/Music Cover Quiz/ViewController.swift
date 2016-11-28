905c4cbb87ebee49d7955c70aec4077da31208ac
Remo Stirnimann <admin@Remos-MacBook-Pro.local>
2016-11-28 19:38:40 +0100

    Start

---
+++ Music Cover Quiz/Music Cover Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music Cover Quiz
+//
+//  Created by Remo Stirnimann on 28.11.16.
+//  Copyright © 2016 Remo Stirnimann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
