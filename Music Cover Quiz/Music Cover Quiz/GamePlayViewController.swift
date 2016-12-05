9abb00e39dbc61390d936edbd85f5b8cc3a50bb2
tfschule <valentin.schuler@stud.hslu.ch>
2016-12-05 21:18:27 +0100

    Kompilliert, tut aber nix.

--- Music Cover Quiz/Music Cover Quiz/GamePlayViewController.swift
+++ Music Cover Quiz/Music Cover Quiz/GamePlayViewController.swift
@@ -19,11 +19,11 @@ class GamePlayViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var labelEnd: UILabel!
     @IBOutlet weak var nextButton: UIButton!
-    @IBOutlet weak var loadedLabel: UILabel!
     @IBOutlet weak var coverImage: UIImageView!
 
+    @IBOutlet weak var labelEnd: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background")!)
@@ -32,19 +32,7 @@ class GamePlayViewController: UIViewController {
         button1.layer.cornerRadius = cornerRadius
         button2.layer.cornerRadius = cornerRadius
         button3.layer.cornerRadius = cornerRadius
-        
-        loadedLabel.isHidden = true
-        
-        let loaded = updateQuizData()
-        quizData = loadQuizDataFromFile()
-        
-        if (loaded) {
-            loadedLabel.isHidden = false
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
-                self.loadedLabel.isHidden = true
-            })
-        }
-        
+
         hideNextButton()
         setupGame()
     }
@@ -58,20 +46,20 @@ class GamePlayViewController: UIViewController {
             }
         }
         
-        let randomIndex = Int(arc4random_uniform(UInt32((quizData?.count)!-1)))
-        while playedQuizData.contains(randomIndex) { }
+ //       let randomIndex = Int(arc4random_uniform(UInt32((quizData?.count)!-1)))
+  //      while playedQuizData.contains(randomIndex) { }
         
-        playedQuizData.append(randomIndex)
+ //       playedQuizData.append(randomIndex)
         
-        let random = Int(arc4random_uniform(UInt32((quizData?.count)!-1)))
+ //       let random = Int(arc4random_uniform(UInt32((quizData?.count)!-1)))
 
         
-        button1.setTitle (quizData?[random].song, for: UIControlState.normal)
-        button2.setTitle ("Karlos", for: UIControlState.normal)
-        button3.setTitle ("William", for: UIControlState.normal)
-        correctAnswer = "2"
+//        button1.setTitle (quizData?[0].song, for: UIControlState.normal)
+//        button2.setTitle ("Karlos", for: UIControlState.normal)
+//        button3.setTitle ("William", for: UIControlState.normal)
+//        correctAnswer = "2"
     
-        playedQuizData.remove(at: randomIndex)
+  //      playedQuizData.remove(at: randomIndex)
     }
     
     func setupNextData() {}
@@ -79,27 +67,27 @@ class GamePlayViewController: UIViewController {
     @IBAction func button1Pressed(_ sender: UIButton) {
         showNextButton()
         if (correctAnswer == "1") {
-            labelEnd.text = "Correcto"
+            labelEnd.text = "Richtig!"
         } else{
-            labelEnd.text = "Falso"
+            labelEnd.text = "Falsch!"
         }
     }
     
     @IBAction func button2Pressed(_ sender: UIButton) {
         showNextButton()
         if (correctAnswer == "2") {
-            labelEnd.text = "Correcto"
+            labelEnd.text = "Richtig!"
         } else{
-            labelEnd.text = "Falso"
+            labelEnd.text = "Falsch!"
         }
     }
     
     @IBAction func button3Pressed(_ sender: UIButton) {
         showNextButton()
         if (correctAnswer == "3") {
-            labelEnd.text = "Correcto"
+            labelEnd.text = "Richtig!"
         } else{
-            labelEnd.text = "Falso"
+            labelEnd.text = "Falsch!"
         }
     }
     
