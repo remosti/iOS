d135485c5be759687e86aa60495910d7942fd98d
Remo Stirnimann <admin@Remos-MacBook-Pro.local>
2016-12-10 00:41:39 +0100

    Klasse nicht committee

--- Music Cover Quiz/Music Cover Quiz/LeaderboardViewController.swift
+++ Music Cover Quiz/Music Cover Quiz/LeaderboardViewController.swift
@@ -8,27 +8,44 @@
 
 import UIKit
 
-class LeaderboardViewController: UIViewController{
-
+class LeaderboardViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
+    
+    
+    
     // MARK: Properties
     let FILENAME = "leaders.xml"
     var leaderBoard : [LeaderBoardEntry] = []
+    var playersName : String = ""
+    var playersScore : Int = -1
     @IBOutlet weak var leaderBoardTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(patternImage: UIImage(named: "Background")!)
+        // Background setzten
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background")!)
+        
+        leaderBoardTable.delegate = self
+        leaderBoardTable.dataSource = self
+        
+        
         // Aktuelle Rangliste aus dem File-Store
         leaderBoard = loadLeaderBoard()
         
-        leaderBoard.append(LeaderBoardEntry(name:"Remo",points:32))
-        //leaderBoard.append("test")
+        // Prüfen, ob ein neuer Eintrag besteht. Wenn neuer Eintrag, zu Array hinzufügen und in File speichern
+        if(playersName != "" && playersScore > -1){
+            leaderBoard.append(LeaderBoardEntry(name: playersName, points: playersScore))
+            storeLeaderBoard()
+        }
+        
+        leaderBoard.append(LeaderBoardEntry(name: "dummy", points: Int(arc4random_uniform(50) + 1)))
         storeLeaderBoard()
+        
+        leaderBoard = sortLeaderBoard(toSort: leaderBoard)
         
         print(leaderBoard.count)
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -44,10 +61,15 @@ class LeaderboardViewController: UIViewController{
         let appPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let filePath = appPath.appendingPathComponent(FILENAME)
         
-        //if leaderBoard != nil{
-            (leaderBoard as NSArray).write(to: filePath, atomically: true)
-        print(appPath.appendingPathComponent(FILENAME) as NSURL	)
-        //}
+        var leaders : [String] = []
+        
+        for item in leaderBoard{
+            if(item.name != "" && item.points > 0){
+                leaders.append("\(item.name);\(item.points)")}
+        }
+        
+        (leaders as NSArray).write(to: filePath, atomically: true)
+        print(filePath as NSURL)
     }
     /**
      * Lesen der aktuellen Rangliste aus dem File-Store
@@ -55,15 +77,54 @@ class LeaderboardViewController: UIViewController{
     func loadLeaderBoard() -> [LeaderBoardEntry]{
         let appPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let filePath = appPath.appendingPathComponent(FILENAME)
-        
         let read = NSArray(contentsOf: filePath)
-        
+        print(filePath as NSURL)
+        var leader : [LeaderBoardEntry] = []
         if read != nil{
-            return read as! [LeaderBoardEntry]
+            for item in read!{
+                let element = (item as! NSString).components(separatedBy: ";")
+                leader.append(LeaderBoardEntry(name: element[0], points: Int(element[1]) ?? 0))
+            }
+            return sortLeaderBoard(toSort: leader)
         }else{
-            return  [LeaderBoardEntry(name:"",points:0)]
+            return  [LeaderBoardEntry(name:"",points: -1)]
         }
     }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = leaderBoardTable.dequeueReusableCell(withIdentifier: "LeaderBoardCell", for: indexPath) as! LeaderBoardTableViewCell
+        
+        if(leaderBoard[indexPath.row].points > -1){
+            cell.labelRang?.text = String(indexPath.row + 1)
+            cell.labelName?.text = leaderBoard[indexPath.row].name
+            cell.labelPunkte?.text = String(leaderBoard[indexPath.row].points)
+        }else{
+            cell.labelRang?.text = ""
+            cell.labelName?.text = ""
+            cell.labelPunkte?.text = ""
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(leaderBoard.count < 10){
+            return leaderBoard.count
+        }else{
+            return 10
+        }
+        
+        
+    }
+    
+    func sortLeaderBoard(toSort: [LeaderBoardEntry]) -> [LeaderBoardEntry]{
+        return toSort.sorted{$0.points > $1.points}
+    }
+    
 }
 
 // MARK: Structur
